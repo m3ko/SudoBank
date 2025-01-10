e753f8c6b4ebb94f6d6b6496d527d543cb23e5cf
Aimar <abengolano@gmail.com>
2025-01-10 10:26:45 +0100

    base de datos con conexiones n a n funciona

---
+++ database/migrations/2025_01_10_085442_tripulantes_viaje.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tripulantes_viaje', function (Blueprint $table) {
+            $table->unsignedBigInteger('viajes_id');
+            $table->unsignedBigInteger('tripulantes_id');
+            $table->foreign('viajes_id')->references('id')->on('viajes');
+            $table->foreign('tripulantes_id')->references('id')->on('tripulantes');
+        });
+
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
