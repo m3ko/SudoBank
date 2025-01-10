a5b4ed50c5c342303924b50290339399fe074a11
Eñaut AA <enaut.irakasle@gmail.com>
2025-01-10 10:59:31 +0100

    controller funciones prueba

--- app/Http/Controllers/TripulantesController.php
+++ app/Http/Controllers/TripulantesController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class TripulantesController extends Controller
 {
-    //
+    public function store(){
+        /*INSTERT*/ 
+    }
+
+    public function  update {
+
+    }
+
+    public function destroy {
+        /*DELETE*/
+
+    }
+
+    public function create {
+
+    }
+
+
+
 }
